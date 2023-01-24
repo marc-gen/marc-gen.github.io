f873b794a50ebd91e83632d3fbce900cb9cd3e69
Marc <mgenereux@cienapps.com>
2023-01-24 17:44:09 -0500

    v21

--- js/accueil.js
+++ js/accueil.js
@@ -1,8 +1,12 @@
-// image d'ordinateur à faire apparaître lorsque l'élément est visible
-let big_ordi = document.querySelector(".mokup-about")
-
-const observer = new IntersectionObserver(e=>{
-    console.log(e[0].isIntersecting)
-}
-)
-observer.observe(big_ordi)
+  // image d'ordinateur à faire apparaître lorsque l'élément est visible
+  let bonjour_titre = document.querySelector("#about-title")
+  let big_ordi = document.querySelector(".mokup-about")
+  
+  const observer = new IntersectionObserver(e=>{
+      console.log(e[0].isIntersecting)
+      if(e[0].isIntersecting == true){
+        big_ordi.classList.add('big-ordi-anim')
+      }
+  }
+  )
+  observer.observe(bonjour_titre )
