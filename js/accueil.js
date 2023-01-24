d830e440ca8a582aea91f4cffbbb649f1f776103
Marc <mgenereux@cienapps.com>
2023-01-24 18:01:23 -0500

    v21

--- js/accueil.js
+++ js/accueil.js
@@ -1,12 +1,3 @@
-  // image d'ordinateur à faire apparaître lorsque l'élément est visible
-  let bonjour_titre = document.querySelector("#about-title")
-  let big_ordi = document.querySelector(".mokup-about")
   
-  const observer = new IntersectionObserver(e=>{
-      console.log(e[0].isIntersecting)
-      if(e[0].isIntersecting == true){
-        big_ordi.classList.add('big-ordi-anim')
-      }
-  }
-  )
-  observer.observe(bonjour_titre )
+
+  
